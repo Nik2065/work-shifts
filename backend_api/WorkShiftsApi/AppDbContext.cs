b22b15362773f387a85e5bae910ae200b4279477
Nik2065 <ivanov_nikolay@bk.ru>
2026-03-29 22:38:32 +0300

    промежуточный комит

--- backend_api/WorkShiftsApi/AppDbContext.cs
+++ backend_api/WorkShiftsApi/AppDbContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using WorkShiftsApi.DbEntities;
 
 namespace WorkShiftsApi
 {
